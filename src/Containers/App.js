d908fc7b4a3cb1d776df485bf210041197ebe176
luis sanchez <sanchezl391@gmail.com>
2019-02-25 20:20:34 -0700

    Initial commit

---
+++ src/Containers/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faIgloo } from '@fortawesome/free-solid-svg-icons'
+// libray.add(faIgloo);
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
